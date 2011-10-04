03284b95d84cc105707e66a174d3225a77876a3c
danielinteract (Daniel Sabanés Bové) <daniel.sabanesbove@gmx.net>
2011-10-04 16:18:00 +0000

    delete everything
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/hypergsplines@9 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/examples/getLogModelPrior.R
+++
@@ -1,9 +0,0 @@
-## artificial configuration:
-config <- c(a=0L, b=1L, c=3L, d=0L)
-
-## get log prior probs:
-getLogModelPrior(config=config,
-                 type="flat")
-
-getLogModelPrior(config=config,
-                 type="exponential")
--- pkg/examples/getRhos.R
+++
@@ -1,9 +0,0 @@
-x <- seq(from=-5,
-         to=5,
-         length=100)
-Z <- makeBasis(x=x,
-               nKnots=4L)
-lambdas <- svd(Z, nu=0L, nv=0L)$d^2
-getRhos(lambdas=lambdas,
-        degrees=c(1, 2, 3, 3.9))
-
