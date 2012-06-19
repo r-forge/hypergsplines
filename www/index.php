6bfd25354d3b0ea6d19a6a8f7d07e03835c572ba
danielinteract (Daniel Sabanés Bové) <daniel.sabanesbove@gmx.net>
2012-06-19 12:57:30 +0000

    First edit of the webpage.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/hypergsplines@19 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -43,9 +43,51 @@ echo $contents; } ?>
 
 <!-- end of project description -->
 
-<p> No content added. </p>
+  <hr>
+  <b>Description:</b>
+  
+  The R-package <tt>hypergsplines</tt> implements objective Bayesian variable
+  selection in generalized additive models (Normal, Binomial and Poisson). As
+  objective parameters prior a (generalized) hyper-g prior is used. As objective
+  model prior so-called multiplicity correction priors are used. More details on
+  the methodology can be found in <a href="http://arxiv.org/abs/1108.3520"> the
+    technical report (2011)</a>.
+  <hr>
+  <b>Download:</b>
 
-<p> The <strong>project summary page</strong> you can find <a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name; ?>/"><strong>here</strong></a>. </p>
+  Currently the R-package <tt>hypergsplines</tt> is not available from CRAN,
+  because it is still under continuing development. The current versions are
+  available from R-Forge (this site). If you are running a recent R version, you
+  can obtain a binary snapshot of the development version as 
+  <div style="text-align:
+	      center;"> 
+    <tt>install.packages("hypergsplines",repos="http://r-forge.r-project.org")</tt>.
+  </div>
+  You can also manually download
+  the <a href="http://r-forge.r-project.org/bin">binary snapshot</a> or
+  the <a href="http://r-forge.r-projec.org/src">source tarball</a>. 
+  You can have a look at the recent changes to the
+  package <a href="https://r-forge.r-project.org/scm/viewvc.php/pkg/ChangeLog?view=markup&root=hypergsplines">here</a>.   
+  The project summary page can be found <a href="http://<?php echo $domain;
+  ?>/projects/<?php echo $group_name; ?>/">here</a>. 
+  <hr>
+  <b>Documentation:</b>
+
+  The R-package features a vignette which introduces the most important functions
+  in a logistic additive regression example. After installing the package, you can
+  open the vignette from within R with the command
+  <div style="text-align:
+	      center;"> 
+    <tt>vignette("examples", package="hypergsplines")</tt>.
+  </div>
+  You can also have a look at the ISBA 2012 <a href="poster.pdf">poster</a>
+  presenting the methodology. 
+  <hr>
+  <b>Developers:</b>
+
+  <a href="http://www.biostat.uzh.ch/aboutus/people/sabanes.html">Daniel Sabanes
+  Bove</a>, Division of Biostatistics, Institute of Social and Preventive
+  Medicine, University of Zurich, Switzerland
 
 </body>
 </html>
